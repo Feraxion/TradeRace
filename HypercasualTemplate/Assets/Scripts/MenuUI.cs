bafd45dc5e536d6285d89898ce65ecd60868ff0e
Suleyman Enes Yavuz <32463690+Feraxion@users.noreply.github.com>
2021-07-09 11:27:51 +0300

    Initial commit

---
+++ HypercasualTemplate/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+       // GameSceneManager.Load(GameSceneManager.Scene.Level1);
+    }
+    
+    //OnClick Button
+    public void StartGame()
+    {
+        GameManager.inst.playerState = GameManager.PlayerState.Playing;
+        GameManager.inst.StartScreen.SetActive(false);
+    }
+
+    public void SkipLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //currentLevelDiamondCount = 0;
+        GameManager.inst.currentLevelDiamondCount = 0;
+        GameManager.inst.bonusMultiplier = 1;
+    }
+
+    public void RestartGame()
+    {
+        Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+    }
+    
+   
+    
+    public void ExitGame()
+    { 
+        Application.Quit();
+    }
+    
+}
