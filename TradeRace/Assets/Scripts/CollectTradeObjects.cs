b71def6f3b3c65d238b1a50cb0dfff5cc5c0b755
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-07-12 14:49:22 +0300

    WIP

--- TradeRace/Assets/Scripts/CollectTradeObjects.cs
+++ TradeRace/Assets/Scripts/CollectTradeObjects.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectTradeObjects : MonoBehaviour
 {
+
+    public int collectedObjects;
+    public Slider playerSlider;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +27,23 @@ public class CollectTradeObjects : MonoBehaviour
         if (other.gameObject.CompareTag("TradeObject"))
         {
             Destroy(other.gameObject);
+            collectedObjects += other.GetComponent<TradeObjectValue>().ItemValue;
+            StartCoroutine(DelaySlider(other.GetComponent<TradeObjectValue>().ItemValue));
         }
     }
+    
+    IEnumerator DelaySlider(int value)
+    {
+
+        for (int j = 0; j < value * 100; j++)
+        {
+
+            playerSlider.value += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+
+        }
+        
+       
+    }
+    
 }
---
+++ TradeRace/Assets/Scripts/GateTeleport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTeleport : MonoBehaviour
+{
+    public GameObject tradeCanvas;
+
+    public GameObject gameplayCamera;
+
+    public GameObject tradeCamera;
+    public GameObject player;
+    public GameObject playerControl;
+    public Transform tradePosition;
+    public GameObject sliderGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.GetComponent<CollectTradeObjects>().collectedObjects >= 5) ;
+            {
+                playerControl.SetActive(false);
+                tradeCamera.SetActive(true);
+                gameplayCamera.SetActive(false);
+                sliderGO.SetActive(false);
+                player.transform.position = tradePosition.position;
+                //tradeCanvas.SetActive(true);
+            }
+            
+        }
+    }
+    
+    
+}
