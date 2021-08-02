57ead3c27872cd307eeaafd88ad01d1a39d6cd0a
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-08-02 15:55:50 +0300

    Push

--- TradeRaceStandard/Assets/Scripts/ButtonCaller.cs
+++ TradeRaceStandard/Assets/Scripts/ButtonCaller.cs
@@ -6,7 +6,8 @@ public class ButtonCaller : MonoBehaviour
 {
     
     MeshRenderer myRenderer;
-    
+    public Material white, green;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,7 @@ public class ButtonCaller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PressButton()
@@ -26,12 +27,15 @@ public class ButtonCaller : MonoBehaviour
 
     IEnumerator ColorChange()
     {
-        Debug.Log("sdfsdf");
         //myRenderer.material.color = Color.Lerp(Color.white,Color.green, 1f );
-        myRenderer.GetComponent<Renderer>().sharedMaterial.SetColor("MainColor", Color.black);
-        //myRenderer.material.SetColor("MainColor",Color.Lerp(Color.white,Color.green, 1f ) );
-        yield return new WaitForSeconds(1f);
+        //myRenderer.GetComponent<Renderer>().material.SetColor("MainColor", Color.black);
         
+        myRenderer.material.Lerp(white,green,1f);
+        //myRenderer.material.SetColor("MainColor",Color.Lerp(Color.white,Color.green, 1f ) );
+        yield return new WaitForSeconds(2f);
+        
+        myRenderer.material.Lerp(green,white,1f);
+
         //myRenderer.material.color = Color.Lerp(Color.green,Color.white, 1f );
         //myRenderer.material.SetColor("MainColor",Color.Lerp(Color.green,Color.white, 1f ) );
 
